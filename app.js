8714fd26d069dc3247c3665e3a37169a899973bf
dotpegaso <dotpegaso@gmail.com>
2017-12-23 17:19:51 -0200

    upload examples

--- app.js
+++ app.js
@@ -16,52 +16,8 @@ app.get('/', function(req, res) {
 five.Board().on('ready', function() {
   console.log('Aight! Arduino conectado! :D');
 
-  // Initial state
-  let state = {
-    red: 1, green: 1, blue: 1
-  };
-
-  // Map pins to digital inputs on the board
-  led = new five.Led.RGB({
-    pins: {
-      red: 6,
-      green: 3,
-      blue: 5
-    }
-  });
-
-  // Helper function to set the colors
-  let setStateColor = function(state) {
-    led.color({
-      red: state.red,
-      blue: state.blue,
-      green: state.green
-    });
-  };
-
-  io.on('connection', function(client) {
-    client.on('join', function(handshake) {
-      console.log(handshake);
-    });
-
-    // Set initial state
-    setStateColor(state);
-
-    client.on('rgb', function(data) {
-      state.red = data.color === 'red' ? data.value : state.red;
-      state.green = data.color === 'green' ? data.value : state.green;
-      state.blue = data.color === 'blue' ? data.value : state.blue;
-
-      // Set the new colors
-      setStateColor(state);
-
-      client.emit('rgb', data);
-      client.broadcast.emit('rgb', data);
-    });
-
-    // Turn on the RGB LED
-    led.on();
-  });
+  //code goes here
+  
 });
 
 const port = process.env.PORT || 3000;
